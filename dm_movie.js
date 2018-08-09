c1626ab7fdcd71c0aa1d8047c01073779982fc90
liuwei@party-time.cn <jushidai123>
2018-08-09 10:49:51 +0800

    动态文字特效

--- dm_movie.js
+++ dm_movie.js
@@ -6,15 +6,29 @@ var last_div_container;
 var current_div_barrager;
 var last_div_barrager;
 
-var showWord = function(word){
-    msgList.push("这是一个测试");
-    msgList.push("这是一个测试123132");
-    msgList.push("这是一个测试456456");
-    msgList.push("这是一个测试789789");
-    msgList.push("这是一个测试012012");
+
+var dWord = function(info,color,size,animate){
+    this.info = info;
+    if( null == color){
+        this.color = '#fff';
+    }else{
+        this.color = color;
+    }
+    this.size = size+'px';
+    this.sizeNum = size;
+    this.animate = animate;
+}
+
+var showWord = function(){
+    msgList.push(new dWord('今天','#fff',120,'bounceInRight'));
+    msgList.push(new dWord("我想对我妈妈说",'#fff',30,'bounceInUp'));
+    msgList.push(new dWord("小时候",'#fff',100,'zoomIn'));
+    msgList.push(new dWord("你为了让我过的更好",'#fff',30,'flipInX'));
+    //msgList.push(new dWord("这是一个测试012012"));
     //msgList.push("这是一个测试234234");
     //msgList.push("hahahaha6666");
-    zoomIn();
+    //zoomIn();
+    drawDWord();
 }
 
 function getRandomNum(Min,Max){
@@ -23,51 +37,39 @@ function getRandomNum(Min,Max){
     return(Min + Math.round(Rand * Range));
 }
 
-var zoomIn = function(){
-    var time = new Date().getTime();
-    var barrager_id = 'barrage_' + time;
-    if(msgList.length>0){
-        var msg = msgList.shift();
-        if( null == current_div_container || msgCount % 3 == 0){
-            last_div_container  = current_div_container;
-             var className = '';
-            if(msgCount==0){
-                className = 'first';
-            }else{
-                className = 'second';
-            }
-            current_div_container = $('<div  id="container_' + barrager_id + '" class="'+className+'">'+msg+'</div>').appendTo($('body'));
-        }
-        ++msgCount;
-        if( null!= current_div_barrager){
-            last_div_barrager = current_div_barrager;
-        }
-        current_div_barrager = $('<div  id="' + barrager_id + '" class="animated rubberBand danmuText">'+msg+'</div>').appendTo(current_div_container);
-        var fontSize = getRandomNum(20,100);
-        current_div_barrager.css('font-size',50);
-        divMsgList.push(current_div_barrager);
-        if(msgCount>0){
-            //for(var i=0;i<divMsgList.length;i++){
-                //divMsgList[i].animate({top:fontSize+"px"},2000);
-           // }
-        }
-        current_div_barrager.on('webkitAnimationEnd', function () {
-            for(var i=0;i<divMsgList.length;i++){
-                var dTop = divMsgList[i].css('top');
-                dTop = dTop.substring(0,dTop.length-2);
-                var moveTop = dTop - 50;
-                divMsgList[i].animate({top:moveTop+"px"},500,'linear',function(){
-                    zoomIn();
-                });
-            }
-        });
-        if(msgCount % 3 == 0){
-            current_div_barrager.on('webkitAnimationEnd', function () {
-                //last_div_container.addClass("anticlockwise");
-            });
-        }
-
+var drawDWord = function(){
+    var dWord = msgList.shift();
+    if( null == current_div_container){
+        current_div_container = $('<div  id="container" class="first"></div>').appendTo($('body'));
     }
-
-
+    var div_b = $('<div class="danmuText animated '+dWord.animate+'">'+dWord.info+'</div>').appendTo(current_div_container);
+    div_b.css('color',dWord.color);
+    div_b.css('font-size',dWord.size);
+    div_b.on('webkitAnimationEnd', function () {
+          //console.log(div_b.html()+' divMsgList.length'+divMsgList.length);
+          divMsgList.push(div_b);
+          if(divMsgList.length > 0 && msgList.length > 0 ){
+                for(var i=0;i<divMsgList.length;i++){
+                    var dTop = divMsgList[i].css('top');
+                    dTop = dTop.substring(0,dTop.length-2);
+                    var moveTop = (dTop - div_b.height())-20;
+                    if( moveTop < 0 ){
+                        moveTop = 0;
+                    }
+                    console.log('moveTop='+moveTop);
+                    //当无法移动时需要缩小当前画面
+                    if( moveTop == 0){
+                        //current_div_container.css("-webkit-transform","scale(0.8)");
+                    }
+                    //判断当向上移动最后一条文字时 需要递归去重新画
+                    if( i == divMsgList.length-1 ){
+                        divMsgList[i].animate({top:moveTop+"px"},500,'linear',function(){
+                            drawDWord();
+                        });
+                    }else{
+                        divMsgList[i].animate({top:moveTop+"px"},500,'linear');
+                    }
+                }
+          }
+    });
 }
